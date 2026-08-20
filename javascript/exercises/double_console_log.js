5945e730b21d88aadbbbc014d02cf0939e0ce173
Hudakova <veronika.hudakova@sky.uk>
2026-08-20 20:31:53 +0100

    add first js files: first scripts, hodnoty-values

---
+++ javascript/exercises/double_console_log.js
@@ -0,0 +1,4 @@
+console.log(
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+);
+console.log(`placeholder`);
---
+++ javascript/values-and-variables/hodnoty.js
@@ -0,0 +1,9 @@
+console.log("string");
+console.log(15); //number bez uvozovek
+console.log(10.01); // !funguje pouze desetinna tecka!
+console.log("12"); //neni cislo ale string
+console.log("12" + "24"); //vysledek 1224
+console.log(true); //boolean - logicka hodnota
+console.log(false);
+console.log(null); //prazdna hodnota - umyslne nastavene na prazdnou hodnotu
+console.log(undefined); //prazdna hodnota - jeste nikdo nenastavil
