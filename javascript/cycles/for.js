747ced9a111f7086dddf6a8eb75c1504508e3cf1
Hudakova <veronika.hudakova@sky.uk>
2026-08-25 11:04:13 +0100

    adding new files exercises for.js and folder cycles

---
+++ javascript/cycles/for.js
@@ -0,0 +1,8 @@
+//vstupni akce, podminka a vystupni akce
+//cislo, ktere nam rika kolikrat to pobezi, pak podminka a pak moniturujeme po kolikate to bezi
+//v zobackove zavorce je blok kodu
+for (let i = 0; i < 10; i++) {
+  console.log("Beh (cyklu) cislo: " + i);
+}
+
+console.log("Uz nebeham");
