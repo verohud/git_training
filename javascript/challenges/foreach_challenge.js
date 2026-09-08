73eb044b2fafcbc23a18a7b17bd03245d7b01a0c
Hudakova <veronika.hudakova@sky.uk>
2026-09-08 11:42:12 +0100

    adding new folders about functions, cycles and arrays, including exercises and challenges

---
+++ javascript/challenges/foreach_challenge.js
@@ -0,0 +1,6 @@
+const firstName = ["Sebastian", "Theo", "Nathaniel", "Stanislav"];
+const lastName = ["Novak", "Hudak", "Novy", "Byrt"];
+
+firstName.forEach((element) => {
+  console.log("first_name + last_name: " + firstName + lastName);
+});
---
+++ javascript/cycles/for_each.js
@@ -0,0 +1,5 @@
+const elements = ["button", "input", "radio", "checkbox"];
+
+elements.forEach((element) => {
+  console.log("Prvek: " + element);
+});
